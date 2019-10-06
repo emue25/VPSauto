3e928bee328f47a80dfdf7d608cb659244820ca7
emue25 <48172339+emue25@users.noreply.github.com>
2019-10-06 11:21:46 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -34,7 +34,7 @@ ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime
 # install webmin
 cd
 wget "https://raw.githubusercontent.com/emue25/VPSauto/master/webmin_1.930_all.deb"
-dpkg --install webmin_1.801_all.deb;
+dpkg --install webmin_1.930_all.deb;
 apt-get -y -f install;
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 rm /root/webmin_1.930_all.deb

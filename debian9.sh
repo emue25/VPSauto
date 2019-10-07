f96106e54de1654cfe2703a541482d293ba3852d
emue25 <48172339+emue25@users.noreply.github.com>
2019-10-07 10:17:47 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 #MODIF BY KOPET
 
+# initializing var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(curl -4 icanhazip.com)
+if [ $MYIP = "" ]; then
+   MYIP=`ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1`;
+fi
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+# initializing var
+#MYIP=`ifconfig eth0 | awk 'NR==2 {print $2}'`
+#MYIP2="s/xxxxxxxxx/$MYIP/g";
+#cd /root
+#wget "https://raw.githubusercontent.com/wangzki03/VPSauto/master/tool/plugin.tgz"
+#wget "https://raw.githubusercontent.com/wangzki03/VPSauto/master/tool/premiummenu.zip"
 
 #repo
 wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
@@ -10,13 +24,6 @@ echo "deb http://build.openvpn.net/debian/openvpn/release/2.4 stretch main" > /e
 apt update
 apt upgrade -y
 apt install openvpn nginx php7.0-fpm stunnel4 squid3 dropbear easy-rsa vnstat ufw build-essential fail2ban zip -y
-
-# initializing var
-MYIP=`ifconfig eth0 | awk 'NR==2 {print $2}'`
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-#cd /root
-#wget "https://raw.githubusercontent.com/wangzki03/VPSauto/master/tool/plugin.tgz"
-#wget "https://raw.githubusercontent.com/wangzki03/VPSauto/master/tool/premiummenu.zip"
 
 # disable ipv6
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
@@ -54,7 +61,122 @@ echo "/bin/false" >> /etc/shells
 /etc/init.d/dropbear resrart
 
 # install squid3
+apt-get -y install squid3
+cat > /etc/squid3/squid.conf <<-END
+acl localhost src 127.0.0.1/32 ::1
+acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 21
+acl Safe_ports port 443
+acl Safe_ports port 70
+acl Safe_ports port 210
+acl Safe_ports port 1025-65535
+acl Safe_ports port 280
+acl Safe_ports port 488
+acl Safe_ports port 591
+acl Safe_ports port 777
+acl CONNECT method CONNECT
+acl SSH dst xxxxxxxxx-xxxxxxxxx/32
+http_access allow SSH
+http_access allow manager localhost
+http_access deny manager
+http_access allow localhost
+http_access deny all
+http_port 8080
+http_port 8000
+http_port 80
+http_port 3128
+coredump_dir /var/spool/squid3
+refresh_pattern ^ftp: 1440 20% 10080
+refresh_pattern ^gopher: 1440 0% 1440
+refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
+refresh_pattern . 0 20% 4320
+visible_hostname daybreakersx
+END
+sed -i $MYIP2 /etc/squid3/squid.conf;
+/etc/init.d/squid restart
+
+# install webserver
 cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+cat > /etc/nginx/nginx.conf <<END3
+user www-data;
+worker_processes 1;
+pid /var/run/nginx.pid;
+events {
+	multi_accept on;
+  worker_connections 1024;
+}
+http {
+	gzip on;
+	gzip_vary on;
+	gzip_comp_level 5;
+	gzip_types    text/plain application/x-javascript text/xml text/css;
+	autoindex on;
+  sendfile on;
+  tcp_nopush on;
+  tcp_nodelay on;
+  keepalive_timeout 65;
+  types_hash_max_size 2048;
+  server_tokens off;
+  include /etc/nginx/mime.types;
+  default_type application/octet-stream;
+  access_log /var/log/nginx/access.log;
+  error_log /var/log/nginx/error.log;
+  client_max_body_size 32M;
+	client_header_buffer_size 8m;
+	large_client_header_buffers 8 8m;
+	fastcgi_buffer_size 8m;
+	fastcgi_buffers 8 8m;
+	fastcgi_read_timeout 600;
+  include /etc/nginx/conf.d/*.conf;
+}
+END3
+mkdir -p /home/vps/public_html
+wget -O /home/vps/public_html/index.html "https://www.sshfast.net/"
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+args='$args'
+uri='$uri'
+document_root='$document_root'
+fastcgi_script_name='$fastcgi_script_name'
+cat > /etc/nginx/conf.d/vps.conf <<END4
+server {
+  listen       85;
+  server_name  127.0.0.1 localhost;
+  access_log /var/log/nginx/vps-access.log;
+  error_log /var/log/nginx/vps-error.log error;
+  root   /home/vps/public_html;
+  location / {
+    index  index.html index.htm index.php;
+    try_files $uri $uri/ /index.php?$args;
+  }
+  location ~ \.php$ {
+    include /etc/nginx/fastcgi_params;
+    fastcgi_pass  127.0.0.1:9000;
+    fastcgi_index index.php;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+  }
+}
+END4
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+/etc/init.d/nginx restart
+
+# install dropbear
+apt-get install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=442/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 110"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+/etc/init.d/dropbear restart
+
+apt-get install zlib1g-dev
+wget https://raw.githubusercontent.com/brantbell/cream/mei/dropbear-2016.74.tar.bz2
+bzip2 -cd dropbear-2016.74.tar.bz2 | tar xvf -
+cd dropbear-2016.74
+./configure
+make && make install
 
 # setting banner
 rm /etc/issue.net
@@ -167,65 +289,107 @@ echo '<ca>' >> /home/vps/public_html/client.ovpn
 cat /etc/openvpn/ca.crt >> /home/vps/public_html/client.ovpn
 echo '</ca>' >> /home/vps/public_html/client.ovpn
 
-cat > /home/vps/public_html/OpenVPN-Stunnel.ovpn <<-END
-# Created by wang zki
-auth-user-pass
+#Create OpenVPN Config
+mkdir -p /home/vps/public_html
+cat > /home/vps/public_html/clientssl.ovpn <<-END
+# OpenVPN sshfast.net
+# modif by zhangzi ovpn ssl
 client
 dev tun
 proto tcp
-remote 127.0.0.1 55
-route $MYIP 255.255.255.255 net_gateway
 persist-key
 persist-tun
+dev tun
 pull
 resolv-retry infinite
 nobind
 user nobody
+group nogroup
 comp-lzo
-remote-cert-tls server
+ns-cert-type server
 verb 3
 mute 2
-connect-retry 5 5
-connect-retry-max 8080
 mute-replay-warnings
+auth-user-pass
 redirect-gateway def1
 script-security 2
-cipher none
-auth none
+route-method exe
+setenv opt block-outside-dns
+route-delay 2
+remote $MYIP 443
+cipher AES-128-CBC
+up /etc/openvpn/update-resolv-conf
+down /etc/openvpn/update-resolv-conf
+route $MYIP 255.255.255.255 net_gateway
 END
-echo '<ca>' >> /home/vps/public_html/OpenVPN-Stunnel.ovpn
-cat /etc/openvpn/ca.crt >> /home/vps/public_html/OpenVPN-Stunnel.ovpn
-echo '</ca>' >> /home/vps/public_html/OpenVPN-Stunnel.ovpn
+echo '<ca>' >> /home/vps/public_html/clientssl.ovpn
+cat /etc/openvpn/ca.crt >> /home/vps/public_html/clientssl.ovpn
+echo '</ca>' >> /home/vps/public_html/clientssl.ovpn
+cd /home/vps/public_html/
+tar -czf /home/vps/public_html/openvpnssl.tar.gz clientssl.ovpn
+tar -czf /home/vps/public_html/clientssl.tar.gz clientssl.ovpn
+cd
+# Restart openvpn
+/etc/init.d/openvpn restart
+#cat > /home/vps/public_html/OpenVPN-Stunnel.ovpn <<-END
+# Created by wang zki
+#auth-user-pass
+#client
+#dev tun
+#proto tcp
+#remote 127.0.0.1 55
+#route $MYIP 255.255.255.255 net_gateway
+#persist-key
+#persist-tun
+#pull
+#resolv-retry infinite
+#nobind
+#user nobody
+#comp-lzo
+#remote-cert-tls server
+#verb 3
+#mute 2
+#connect-retry 5 5
+#connect-retry-max 8080
+#mute-replay-warnings
+#redirect-gateway def1
+#script-security 2
+#cipher none
+#auth none
+#END
+#echo '<ca>' >> /home/vps/public_html/OpenVPN-Stunnel.ovpn
+#cat /etc/openvpn/ca.crt >> /home/vps/public_html/OpenVPN-Stunnel.ovpn
+#echo '</ca>' >> /home/vps/public_html/OpenVPN-Stunnel.ovpn
 
-cat > /home/vps/public_html/stunnel.conf <<-END
-client = yes
-debug = 6
-[openvpn]
-accept = 127.0.0.1:55
-connect = $MYIP:587
-TIMEOUTclose = 0
-verify = 0
-sni = m.facebook.com
-END
+#cat > /home/vps/public_html/stunnel.conf <<-END
+#client = yes
+#debug = 6
+#[openvpn]
+#accept = 127.0.0.1:55
+#connect = $MYIP:587
+#TIMEOUTclose = 0
+#verify = 0
+#sni = m.facebook.com
+#END
 
 # Configure Stunnel
-sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -sha256 -subj '/CN=127.0.0.1/O=localhost/C=PH' -keyout /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.pem
-cat > /etc/stunnel/stunnel.conf <<-END
-sslVersion = all
-pid = /stunnel.pid
-socket = l:TCP_NODELAY=1
-socket = r:TCP_NODELAY=1
-client = no
-[openvpn]
-accept = 587
-connect = 127.0.0.1:55
-cert = /etc/stunnel/stunnel.pem
-[dropbear]
-accept = 443
-connect = 127.0.0.1:442
-cert = /etc/stunnel/stunnel.pem
-END
+#sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+#openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -sha256 -subj '/CN=127.0.0.1/O=localhost/C=PH' -keyout /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.pem
+#cat > /etc/stunnel/stunnel.conf <<-END
+#sslVersion = all
+#pid = /stunnel.pid
+#socket = l:TCP_NODELAY=1
+#socket = r:TCP_NODELAY=1
+#client = no
+#[openvpn]
+#accept = 587
+#connect = 127.0.0.1:55
+#cert = /etc/stunnel/stunnel.pem
+#[dropbear]
+#accept = 443
+#connect = 127.0.0.1:442
+#cert = /etc/stunnel/stunnel.pem
+#END
 
 #Setting UFW
 ufw allow ssh
@@ -286,8 +450,8 @@ sed -i $MYIP2 /etc/iptables.up.rules;
 iptables-restore < /etc/iptables.up.rules
 
 # Configure Nginx
-sed -i 's/\/var\/www\/html;/\/home\/vps\/public_html\/;/g' /etc/nginx/sites-enabled/default
-cp /var/www/html/index.nginx-debian.html /home/vps/public_html/index.html
+#sed -i 's/\/var\/www\/html;/\/home\/vps\/public_html\/;/g' /etc/nginx/sites-enabled/default
+#cp /var/www/html/index.nginx-debian.html /home/vps/public_html/index.html
 
 
 # Create and Configure rc.local
@@ -307,18 +471,61 @@ wget "https://raw.githubusercontent.com/wangzki03/VPSauto/master/tool/premiummen
 unzip premiummenu.zip
 chmod +x /usr/local/bin/*
 
-# add eth0 to vnstat
-vnstat -u -i eth0
+#vnstat
+cd /home/vps/public_html/
+wget https://raw.githubusercontent.com/brantbell/cream/mei/vnstat_php_frontend-1.5.1.tar.gz
+
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+cd
 
 # compress configs
 cd /home/vps/public_html
-zip configs.zip client.ovpn OpenVPN-Stunnel.ovpn stunnel.conf
+zip configs.zip client.ovpn clientssl.ovpn
 
 # install libxml-parser
 apt-get install -y libxml-parser-perl
+# swap ram
+dd if=/dev/zero of=/swapfile bs=1024 count=4096k
+# buat swap
+mkswap /swapfile
+# jalan swapfile
+swapon /swapfile
+#auto star saat reboot
+wget https://raw.githubusercontent.com/brantbell/cream/mei/fstab
+mv ./fstab /etc/fstab
+chmod 644 /etc/fstab
+sysctl vm.swappiness=10
+#permission swapfile
+chown root:root /swapfile 
+chmod 0600 /swapfile
+cd
+#ssl 0vpn
+sudo apt update
+sudo apt full-upgrade
+sudo apt install -y stunnel4
+cd /etc/stunnel/
+openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -sha256 -subj '/CN=127.0.0.1/O=localhost/C=US' -keyout /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.pem
+sudo touch stunnel.conf
+echo "client = no" | sudo tee -a /etc/stunnel/stunnel.conf
+echo "[openvpn]" | sudo tee -a /etc/stunnel/stunnel.conf
+echo "accept = 443" | sudo tee -a /etc/stunnel/stunnel.conf
+echo "connect = 127.0.0.1:55" | sudo tee -a /etc/stunnel/stunnel.conf
+echo "cert = /etc/stunnel/stunnel.pem" | sudo tee -a /etc/stunnel/stunnel.conf
 
+sudo sed -i -e 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+sudo cp /etc/stunnel/stunnel.pem ~
+# download stunnel.pem from home directory. It is needed by client.
+/etc/init.d/stunnel4 restart
 # finalizing
-vnstat -u -i eth0
+/etc/init.d/vnstat restart
 apt-get -y autoremove
 chown -R www-data:www-data /home/vps/public_html
 /etc/init.d/nginx start

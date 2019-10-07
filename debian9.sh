0246196c1b17f8abe20301e7c78c6b6e6790fcff
emue25 <48172339+emue25@users.noreply.github.com>
2019-10-07 21:25:20 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -183,7 +183,7 @@ make && make install
 
 # setting banner
 rm /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/wangzki03/premscript/master/issue.net"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/brantbell/cream/mei/bannerssh"
 sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 /etc/init.d/ssh restart

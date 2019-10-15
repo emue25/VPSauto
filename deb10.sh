fe8e106602635921d2377d269724f29d0fb67910
emue25 <48172339+emue25@users.noreply.github.com>
2019-10-15 21:31:33 +0800

    Update deb10.sh

--- deb10.sh
+++ deb10.sh
@@ -3,10 +3,12 @@
 # initializing var
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
+
 cd /root
 wget "https://github.com/johndesu090/AutoScriptDebianStretch/raw/master/Files/Plugins/plugin.tgz"
 wget "https://github.com/johndesu090/AutoScriptDebianStretch/raw/master/Files/Menu/bashmenu.zip"
 
+#repo
 wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
 sleep 2
 echo "deb http://build.openvpn.net/debian/openvpn/release/2.4 boster main" > /etc/apt/sources.list.d/openvpn-aptrepo.list

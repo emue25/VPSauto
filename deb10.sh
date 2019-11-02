a814c9c3c6710353d85201570e1e4bf93745df0f
emue25 <48172339+emue25@users.noreply.github.com>
2019-11-02 10:32:19 +0800

    Update deb10.sh

--- deb10.sh
+++ deb10.sh
@@ -11,7 +11,7 @@ tar -xzvf plugin.tgz
 #repo
 wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
 #sleep 2
-echo "deb http://build.openvpn.net/debian/openvpn/release/3.0.5 buster main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
+echo "deb http://build.openvpn.net/debian/openvpn/release/2.4/dists/buster-20191031090517419242087/main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
 #Requirement
 apt update
 apt upgrade -y

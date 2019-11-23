eff440ddc168b6ab2c5618196e9cdd828d8d1095
emue25 <48172339+emue25@users.noreply.github.com>
2019-11-23 10:45:55 +0800

    Update deb10.sh

--- deb10.sh
+++ deb10.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Script by weduz
-#repo
+
 sudo -s
 wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
 echo "deb http://build.openvpn.net/debian/openvpn/release/2.4 buster main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
